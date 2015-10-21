aeaf225a274feaa41e35c7517e760bc29fad7e48
Jonah George <jonah.george@me.com>
2015-10-20 17:35:33 -0700

    Convert hash definition to use symbol keys

--- examples/whiskas_model2.rb
+++ examples/whiskas_model2.rb
@@ -6,11 +6,11 @@
 require "rulp"
 
 ingredients     = [Chicken_i, Beef_i, Mutton_i, Rice_i, Wheat_i, Gel_i]
-costs           = {"Chicken": 0.013, "Beef": 0.008, "Mutton": 0.010, "Rice": 0.002, "Wheat": 0.005, "Gel": 0.001}
-protein_percent = {"Chicken": 0.100, "Beef": 0.200, "Mutton": 0.150, "Rice": 0.000, "Wheat": 0.040, "Gel": 0.000}
-fat_percent     = {"Chicken": 0.080, "Beef": 0.100, "Mutton": 0.110, "Rice": 0.010, "Wheat": 0.010, "Gel": 0.000}
-fibre_percent   = {"Chicken": 0.001, "Beef": 0.005, "Mutton": 0.003, "Rice": 0.100, "Wheat": 0.150, "Gel": 0.000}
-salt_percent    = {"Chicken": 0.002, "Beef": 0.005, "Mutton": 0.007, "Rice": 0.002, "Wheat": 0.008, "Gel": 0.000}
+costs           = {Chicken: 0.013, Beef: 0.008, Mutton: 0.010, Rice: 0.002, Wheat: 0.005, Gel: 0.001}
+protein_percent = {Chicken: 0.100, Beef: 0.200, Mutton: 0.150, Rice: 0.000, Wheat: 0.040, Gel: 0.000}
+fat_percent     = {Chicken: 0.080, Beef: 0.100, Mutton: 0.110, Rice: 0.010, Wheat: 0.010, Gel: 0.000}
+fibre_percent   = {Chicken: 0.001, Beef: 0.005, Mutton: 0.003, Rice: 0.100, Wheat: 0.150, Gel: 0.000}
+salt_percent    = {Chicken: 0.002, Beef: 0.005, Mutton: 0.007, Rice: 0.002, Wheat: 0.008, Gel: 0.000}
 
 objective = ingredients.map{|i| costs[i.name] * i}.inject(:+)
 problem = Rulp::Min(objective) 

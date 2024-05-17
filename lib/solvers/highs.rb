e9679de3f40f0eaa58fc2ce671756ab4b85b6122
Tomasz Jóźwik <hello@tjozwik.eu>
2024-05-17 03:00:11 +0200

    Handle HiGHS options file

--- lib/solvers/highs.rb
+++ lib/solvers/highs.rb
@@ -1,10 +1,21 @@
 class Highs < Solver
+  HIGHS_SUPPORTED_OPTIONS = {
+    gap: :mip_rel_gap
+  }.freeze
+
   def solve
-    command = "#{executable} --model_file #{@filename} --solution_file #{@outfile}"
+    options_file = create_highs_options_file
+
+    command = "#{executable} --model_file #{@filename} --solution_file #{@outfile} %s %s"
     command %= [
-      options[:time_limit] ? "--time_limit #{options[:time_limit]}" : ''
+      options[:time_limit] ? "--time_limit #{options[:time_limit]}" : '',
+      options_file.length.positive? ? "--options_file #{options_file}" : ''
     ]
+
     exec(command)
+
+    # Remove options file as HiGHS requires additional params in file format instead of command line arguments
+    FileUtils.rm(options_file) unless options_file.empty?
   end
 
   def self.executable
@@ -35,5 +46,24 @@ class Highs < Solver
     end
 
     rows.find { |r| r.match?(/Objective/) }.to_s.split(/\s+/).last.to_f
+  end
+
+  private
+
+  def create_highs_options_file
+    options_str = ''
+
+    HIGHS_SUPPORTED_OPTIONS.each do |rulp_key, highs_key|
+      next unless options[rulp_key]
+
+      options_str += "#{highs_key} = #{options[rulp_key]}\n"
+    end
+
+    return '' if options_str.empty?
+
+    options_file = "/tmp/highs-#{Random.rand(0..1_000_000)}.opt"
+    IO.write(options_file, options_str)
+
+    options_file
   end
 end
